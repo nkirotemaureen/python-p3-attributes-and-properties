20c1dc257fef3e23247a7bd68184e274f54ba23c
+ <+>
2025-05-28 10:02:41 +0300

    attr

--- lib/person.py
+++ lib/person.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 APPROVED_JOBS = [
@@ -16,4 +17,43 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    approved_jobs = [
+    "Admin",
+    "Customer Service",
+    "Human Resources",
+    "ITC",
+    "Production",
+    "Legal",
+    "Finance",
+    "Sales",
+    "General Management",
+    "Research & Development",
+    "Marketing",
+    "Purchasing"
+]
+    def __init__(self,name="unknown",job="unknown"):
+        self._name=None
+        self._job=None
+        self.name=name
+        self.job=job
+        
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.") 
+    @property
+    def job(self):
+        return self._job
+    @job.setter
+    def job(self,job):
+        if job in self.approved_jobs:
+            self._job=job
+        else:
+             print("Job must be in list of approved jobs.")
+    
